e9ed72a35963cd35e74369388eb1f3b3d506a63a
Kostiantyn Hromovyi <ydydydydydyyd@gmail.com>
2024-02-27 19:24:14 +0200

    wrote Genre model

--- app/Models/Genre.php
+++ app/Models/Genre.php
@@ -4,8 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Genre extends Model
 {
     use HasFactory;
+
+    public $timestamps = false;
+
+    protected $fillable = ['name'];
+
+    public function movies(): BelongsToMany
+    {
+        return $this->belongsToMany(Movie::class, 'movies_genres');
+    }
 }
