971d5df3555ae2d389db7a3d9ea33b3b4062878a
Kostiantyn Hromovyi <ydydydydydyyd@gmail.com>
2024-02-28 01:27:05 +0200

    create MovieResource

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\GenreController;
+use App\Http\Controllers\MovieController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::apiResources([
+    'movie' => MovieController::class,
+    'genre' => GenreController::class,
+]);
+
+
