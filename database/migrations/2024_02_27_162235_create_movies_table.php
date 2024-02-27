fdbfcc6bcfde1671ba8c834197baf00ca8279bc7
Kostiantyn Hromovyi <ydydydydydyyd@gmail.com>
2024-02-27 18:38:22 +0200

    wrote movies migration file

--- database/migrations/2024_02_27_162235_create_movies_table.php
+++ database/migrations/2024_02_27_162235_create_movies_table.php
@@ -13,7 +13,9 @@ return new class extends Migration
     {
         Schema::create('movies', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('title', 180)->unique();
+            $table->boolean('is_published')->default(false);
+            $table->string('image')->nullable();
         });
     }
 
