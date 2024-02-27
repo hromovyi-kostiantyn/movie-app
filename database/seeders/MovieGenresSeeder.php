d120f4308bb6ec14f2335a6090b28121cb4ad13b
Kostiantyn Hromovyi <ydydydydydyyd@gmail.com>
2024-02-27 18:58:10 +0200

    created movie,gengre and movie genres seeders

---
+++ database/seeders/MovieGenresSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class MovieGenresSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // use movie genres factory after creating model
+    }
+}
