9c70b273427cbef162dfdb08a0e8add87c9a7bba
Kostiantyn Hromovyi <ydydydydydyyd@gmail.com>
2024-02-27 18:49:58 +0200

    wrote movie genres migration file

--- database/migrations/2024_02_27_162737_create_movie_genres_table.php
+++ database/migrations/2024_02_27_162737_create_movie_genres_table.php
@@ -13,7 +13,8 @@ return new class extends Migration
     {
         Schema::create('movie_genres', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->foreignId('movie_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('genre_id')->constrained()->cascadeOnDelete();
         });
     }
 
