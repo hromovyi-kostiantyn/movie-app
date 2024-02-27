7016ba59201c610e9096b46882029fee6af71356
Kostiantyn Hromovyi <ydydydydydyyd@gmail.com>
2024-02-27 19:27:07 +0200

    wrote Movie model

--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Movie extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['title', 'is_published', 'image'];
+
+//    protected $attributes = [
+//        'image' => 'path to default image',
+//    ];
+
+    public function genres(): BelongsToMany
+    {
+        return $this->belongsToMany(Genre::class, 'movies_genres');
+    }
 }
