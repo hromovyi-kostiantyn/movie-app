0e7267df80b4ef853953c661766e58c10005046e
Kostiantyn Hromovyi <ydydydydydyyd@gmail.com>
2024-02-28 14:55:41 +0200

    create MovieResource

--- app/Http/Resources/MovieResource.php
+++ app/Http/Resources/MovieResource.php
@@ -8,12 +8,18 @@ use Illuminate\Http\Resources\Json\JsonResource;
 class MovieResource extends JsonResource
 {
     /**
-     * Transform the resource into an array.
+     * Transform the resource collection into an array.
      *
-     * @return array<string, mixed>
+     * @return array<int|string, mixed>
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'is_published' => $this->is_published,
+            'image' => $this->image,
+            'genres' => GenreResource::collection($this->whenLoaded('genres')),
+        ];
     }
 }
