31edbf65669cbafd4eabcb116eca9b2ee3932bff
Kostiantyn Hromovyi <ydydydydydyyd@gmail.com>
2024-02-29 16:30:11 +0200

    added GenreController

--- app/Http/Controllers/GenreController.php
+++ app/Http/Controllers/GenreController.php
@@ -2,47 +2,74 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\DTO\GenreDTO;
+use App\Http\Requests\GenreRequest;
+use App\Http\Resources\GenreResource;
+use App\Http\Resources\MovieResource;
+use App\Models\Genre;
+use App\Service\GenreService;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Resources\Json\JsonResource;
 
 class GenreController extends Controller
 {
+    private GenreService $genreService;
+
+    public function __construct(GenreService $genreService) {
+        $this->genreService = $genreService;
+    }
+
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): JsonResource
     {
-        //
+        return GenreResource::collection(Genre::all());
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(GenreRequest $request): GenreResource
     {
-        //
+        $data = $request->validated();
+
+        $genreDTO = GenreDTO::fromValidatedData($data);
+
+        $genre = $this->genreService->store($genreDTO);
+
+        return GenreResource::make($genre);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Genre $genre): JsonResource
     {
-        //
+        return MovieResource::collection($genre->movies()->simplePaginate(5));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(GenreRequest $request, Genre $genre): GenreResource
     {
-        //
+        $data = $request->validated();
+
+        $genreDTO = GenreDTO::fromValidatedData($data);
+
+        $this->genreService->update($genreDTO,$genre);
+
+        return GenreResource::make($genre);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Genre $genre): JsonResponse
     {
-        //
+        $this->genreService->delete($genre);
+
+        return response()->json(['message' => 'Genre deleted'], 200);
     }
 }
