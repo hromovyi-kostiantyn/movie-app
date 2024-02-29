7ac6a89dcfcf8da087f6b4391576d72bf792ea2f
Kostiantyn Hromovyi <ydydydydydyyd@gmail.com>
2024-02-29 16:30:22 +0200

    added MovieController

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -2,47 +2,89 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\DTO\MovieDTO;
+use App\Http\Requests\StoreMovieRequest;
+use App\Http\Requests\UpdateMovieRequest;
+use App\Http\Resources\GenreResource;
+use App\Http\Resources\MovieResource;
+use App\Models\Movie;
+use App\Service\MovieService;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Resources\Json\JsonResource;
 
 class MovieController extends Controller
 {
+    private MovieService $movieService;
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function __construct(MovieService $movieService) {
+        $this->movieService = $movieService;
+    }
+
+
+    public function index(): JsonResource
     {
-        //
+        return MovieResource::collection(Movie::simplePaginate(5));
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreMovieRequest $request): GenreResource
     {
-        //
+
+        $data = $request->validated();
+
+        $movieDTO = MovieDTO::fromStoreValidatedData($data);
+
+        $movie = $this->movieService->store($movieDTO);
+
+        return GenreResource::make($movie);
     }
+
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Movie $movie): JsonResource
     {
-        //
+        return MovieResource::make($movie);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(UpdateMovieRequest $request, Movie $movie): JsonResponse
     {
-        //
+        if (!$request->hasAny(['title', 'image', 'genres', 'is_published'])) {
+            return response()->json(['message' => 'No data to update'], 422);
+        }
+
+        $data = $request->validated();
+
+        $movieDTO = MovieDTO::fromUpdateValidatedData($data);
+
+        $this->movieService->update($movieDTO,$movie);
+
+        return response()->json(['message' => 'Movie updated'], 200);
     }
+
+    public function publish(Movie $movie): JsonResponse {
+        if ($movie->is_published) {
+            return response()->json(['message' => 'Movie already published'], 422);
+        }
+        $this->movieService->publish($movie);
+        return response()->json(['message' => 'Movie published'], 200);
+    }
+
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Movie $movie): JsonResponse
     {
-        //
+        $this->movieService->delete($movie);
+        return response()->json(['message' => 'Movie deleted'], 200);
     }
 }
